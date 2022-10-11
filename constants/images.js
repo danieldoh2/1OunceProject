af69968bc95055904dc464bd1b3e2c002be94ea2
danieldoh2 <109438366+danieldoh2@users.noreply.github.com>
2022-10-10 20:37:44 -0400

    Add files via upload

---
+++ constants/images.js
@@ -0,0 +1,21 @@
+const loginBackground = require("../assets/images/system/login-background.png")
+
+const recipe = require("../assets/images/recipes/recipe.png")
+const spagetti = require("../assets/images/recipes/spagetti.png")
+const satay = require("../assets/images/recipes/satay.png")
+const laksa = require("../assets/images/recipes/laksa.png")
+const nasiLemak = require("../assets/images/recipes/nasi-lemak.webp")
+
+const profile = require("../assets/images/dummy_profiles/profile.png")
+const UserProfile1 = require("../assets/images/dummy_profiles/profile-pic-1.png")
+
+export default {
+    loginBackground,
+    profile,
+    recipe,
+    spagetti,
+    satay,
+    laksa,
+    nasiLemak,
+
+}
