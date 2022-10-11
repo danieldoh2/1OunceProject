b80d8eb202ac287a2169bb514eb2e6b792f92892
danieldoh2 <109438366+danieldoh2@users.noreply.github.com>
2022-10-10 20:24:22 -0400

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,48 @@
+// import { Header } from '@react-navigation/stack';
+// import React from 'react';
+
+// import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+// import { ScrollView } from 'react-native-gesture-handler';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { checkPluginState } from 'react-native-reanimated/lib/reanimated2/core';
+// import { color } from 'react-native-reanimated';
+
+// export default function ProfileScreen() {
+//     return (
+//         <SafeAreaView style={styles.container}>
+//             <ScrollView showsHorizontalScrollIndicator={false}>
+//                 <View style={{ alignSelf: 'center' }}>
+//                     <View style={styles.profileImage}>
+//                         {/* <Image source={require('./assets/images/profile-pic-2.png')}></Image> */}
+//                     </View>
+//                 </View>
+
+//             </ScrollView>
+//         </SafeAreaView>
+//     )
+// }
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: 'white',
+//         alignItems: 'center',
+//         justifyContent: "center"
+//     },
+//     text: {
+//         fontFamily: "HelveticaNeue",
+//         color: 'white'
+//     },
+//     image: {
+//         flex: 1,
+//         width: undefined,
+//         height: undefined
+//     },
+//     profileImage: {
+//         width: 200,
+//         height: 200,
+//         borderRadius: 100,
+//         overflow: 'hidden'
+//     }
+// }
+
+// )
